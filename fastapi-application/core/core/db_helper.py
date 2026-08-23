1934eb418d40470ae366c540e1220d8559a23a99
KalievNurkanat <kalievnurkanat89@gmail.com>
2026-08-23 19:38:54 +0600

    new db helpers

---
+++ fastapi-application/core/core/db_helper.py
@@ -0,0 +1,45 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from core.config import settings
+
+
+class DB_Helper:
+    def __init__(
+            self,
+            url: str,
+            echo: bool,
+            echo_pool: bool,
+            max_overflow: int = 10
+    ):
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            max_overflow=max_overflow
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False
+        )
+
+    async def dispose(self):
+        await self.engine.dispose()
+
+    async def session_getter(self):
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DB_Helper(
+    url = settings.db.url,
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow
+)
