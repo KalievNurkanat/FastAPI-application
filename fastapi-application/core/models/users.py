145f134eec64646010dab3472d7bef832281f8b6
KalievNurkanat <kalievnurkanat89@gmail.com>
2026-08-30 19:05:59 +0600

    making access token

--- fastapi-application/core/models/users.py
+++ fastapi-application/core/models/users.py
@@ -1,8 +1,14 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import Base
+if TYPE_CHECKING:
+    from core.models import Base
 
+class User(Base, SQLAlchemyBaseUserTable[int]):
 
-class User(Base, SQLAlchemyBaseUserTable):
-    pass
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return cls(SQLAlchemyUserDatabase(session, cls))
 
