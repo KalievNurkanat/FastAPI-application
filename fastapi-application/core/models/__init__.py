a72fdec135e7b761aba364be7a73ae59da61e5e2
KalievNurkanat <kalievnurkanat89@gmail.com>
2026-09-01 23:39:33 +0600

    some changes

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,11 +1,9 @@
 __all__ = (
-    "AccessToken",
     "Base",
     "User",
     "db_helper"
 )
 
-from .access_token import AccessToken
 from .base import Base
 from .db_helper import db_helper
 from .users import User
